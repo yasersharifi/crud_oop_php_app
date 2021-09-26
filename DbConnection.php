d00807d7f7bc73a7e62399ef7b2a3bb8de3ffca0
yasersharifi <yassersharifi74@gmail.com>
2021-09-26 19:38:38 +0330

    create connection class

---
+++ DbConnection.php
@@ -0,0 +1,29 @@
+<?php
+class DbConnection {
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->host = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->database = "employee_oop";
+
+        if (! $this->connection) {
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+            if ($this->connection->error) {
+                return $this->connection;
+            }
+            return false;
+        }
+
+        return $this->connection;
+
+    }
+}
--- template/header.php
+++ template/header.php
@@ -56,7 +56,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
                 </li>
-          
+
                 <li class="nav-item">
                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
