857277440e5eb991eef1df8622ac81ed5542a67b
yasersharifi <yassersharifi74@gmail.com>
2021-09-26 19:56:08 +0330

    show all employee in index.php file

--- Users.php
+++ Users.php
@@ -1,4 +1,22 @@
 <?php
+require_once "DbConnection.php";
 class Users extends DbConnection {
-    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get() {
+        $users = null;
+        $sql = "SELECT * FROM users";
+        $result = $this->connection->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $users[] = (object) $row;
+            }
+            return $users;
+        }
+        return false;
+    }
 }
